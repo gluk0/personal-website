347a318086342ab3d22fbb662e3e0b3b091fd23a
Rich Clarke <richyclarke2@gmail.com>
2025-02-07 21:56:49 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,17 +11,13 @@ terraform {
   }
 }
 
-
-
 provider "google" {
   project = var.project_id
   region  = var.region
 }
 
-
 module "artifact_registry" {
   source = "./modules/artifact_registry"
-
   project_id   = var.project_id
   region       = var.region
   repository_id = var.repository_id
@@ -29,7 +25,6 @@ module "artifact_registry" {
 
 module "cloud_run" {
   source = "./modules/cloud_run"
-
   project_id    = var.project_id
   region        = var.region
   service_name  = var.service_name
