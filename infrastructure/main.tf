0f77ec687c2e4cc8d0be703b7111b83a8b5e8e3b
Richard Clarke <richardaclarke@outlook.com>
2025-02-07 22:00:18 +0000

    add gcp apis

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,12 +16,20 @@ provider "google" {
   region  = var.region
 }
 
+module "google_apis" {
+  source = "./modules/google_apis"
+
+  project_id = var.project_id
+}
+
 module "artifact_registry" {
   source = "./modules/artifact_registry"
 
-  project_id   = var.project_id
-  region       = var.region
-  repository_id = var.repository_id
+  project_id     = var.project_id
+  region         = var.region
+  repository_id  = var.repository_id
+  
+  depends_on = [module.google_apis]
 }
 
 module "cloud_run" {
@@ -31,4 +39,6 @@ module "cloud_run" {
   region        = var.region
   service_name  = var.service_name
   image_name    = var.image_name
+  
+  depends_on = [module.google_apis]
 } 
