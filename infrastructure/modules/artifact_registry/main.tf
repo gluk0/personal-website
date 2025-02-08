0a47f28929fc7c115110bb31e4094314a9dce4d4
Rich Clarke <richyclarke2@gmail.com>
2025-02-08 08:26:09 +0000

    chore: Remove IAM binding

--- infrastructure/modules/artifact_registry/main.tf
+++ infrastructure/modules/artifact_registry/main.tf
@@ -8,11 +8,3 @@ resource "google_artifact_registry_repository" "portfolio" {
     immutable_tags = true
   }
 }
-
-# IAM binding for Cloud Run to pull images
-resource "google_artifact_registry_repository_iam_member" "cloud_run_pull" {
-  location   = google_artifact_registry_repository.portfolio.location
-  repository = google_artifact_registry_repository.portfolio.name
-  role       = "roles/artifactregistry.reader"
-  member     = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
-} 
